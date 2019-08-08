40610dbd8cf4453622b5143f9e10803d78695c47
mverost <mverost4422@gmail.com>
2019-08-08 01:04:40 -0400

    Consolidated sub nav and main nav for UI purposes. Adjusted the top margin of each page to work with the new nav bar. Improved upon responsive style in Services and Testimonials components.

--- src/Layout/Testimonials.js
+++ src/Layout/Testimonials.js
@@ -9,16 +9,16 @@ class Testimonials extends Component {
     return (
       <>
         <div className="testimonial-page-container">
-            <h1>Testimonials</h1>
           <div className="testimonials-container">
-          <Carousel
-            infiniteLoop
-            autoPlay
-            showStatus={false}
-            showIndicators={false}
-            showThumbs={false}
-            className="presentation-mode"
-          >
+            <h1>Testimonials</h1>
+            <Carousel
+              infiniteLoop
+              autoPlay
+              showStatus={false}
+              showIndicators={false}
+              showThumbs={false}
+              className="presentation-mode"
+            >
               <div className="slide primary">
                 <p>Roger has completed the job in an unbelievable speed. I hired him for a lawn mowing and I am very satisfied of his service. I will continue use him as our service provider. He is very easy to communicate and responsible. Highly recommended!</p>
                 <h3>Yu Ren C.<br/>Malden, MA<br/>-Yelp</h3>
@@ -31,8 +31,8 @@ class Testimonials extends Component {
                 <p>Roger just did a great job raking and cleaning up our lawn. He and his crew were punctual and did a fantastic job.  I would absolutely use them again. We were running errands during the time they were working, so they sent pics throughout the job to make sure we were seeing what we wanted from their work.</p>
                 <h3>Jessie M.<br/>Arlington, Boston, MA<br/>- Yelp</h3>
               </div>
-          </Carousel>
-          <a target="_blank" rel="noopener noreferrer" href="https://www.yelp.com/biz/rogers-snow-removal-and-landscaping-service-boston-3">Leave a review on Yelp</a>
+            </Carousel>
+            <a target="_blank" rel="noopener noreferrer" href="https://www.yelp.com/biz/rogers-snow-removal-and-landscaping-service-boston-3">Leave a review on Yelp</a>
           </div>
         </div>
       </>
