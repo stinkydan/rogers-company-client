12d5bf121068416b7486aec52fa5b9fcb6685838
mverost <mverost4422@gmail.com>
2019-12-28 12:35:27 -0500

    deploy task

---
+++ precache-manifest.d4e458d4f1d990e9d98467b21dc088a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8379fde762b1db9df00d039eac2bec3",
+    "url": "/static/media/cut-grass.a8379fde.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "24cd05e360e24ddc3c3f",
+    "url": "/static/js/main.885f7ea3.chunk.js"
+  },
+  {
+    "revision": "f828ca5ff12281d47938",
+    "url": "/static/js/2.1b9e899d.chunk.js"
+  },
+  {
+    "revision": "24cd05e360e24ddc3c3f",
+    "url": "/static/css/main.680133df.chunk.css"
+  },
+  {
+    "revision": "f828ca5ff12281d47938",
+    "url": "/static/css/2.0b15a2cb.chunk.css"
+  },
+  {
+    "revision": "a331fb826745168025b6c614b7273631",
+    "url": "/index.html"
+  }
+];
